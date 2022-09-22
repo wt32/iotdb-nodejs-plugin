da06d60dc6e82a164d08dc5e496f1fb7356eb5dd
wt32 <1342534809@qq.com>
2022-09-22 11:07:49 +0800

    Update use.js

--- test/use.js
+++ test/use.js
@@ -1,5 +1,5 @@
 const { Iotdb } = require("../build/Release/hello-world-native.node");
 
-const iotdb = new Iotdb("192.168.61.186", 6667, "root", "root");
+const iotdb = new Iotdb("127.0.0.1", 6667, "root", "root");
 
 iotdb.query("SHOW STORAGE GROUP;");
