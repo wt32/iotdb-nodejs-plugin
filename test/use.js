128799c890b10bdc624b7cc22c80000cf8350ff1
wt32 <1342534809@qq.com>
2022-10-03 00:29:01 +0800

    优化查询返回格式

--- test/use.js
+++ test/use.js
@@ -2,10 +2,5 @@ const {Iotdb} = require("./mac/iotdb.node");
 
 const iotdb = new Iotdb("127.0.0.1", 6667, "root", "root");
 
-// let storage = iotdb.query("SHOW STORAGE GROUP;");
-// console.log(storage)
 let res = iotdb.query("show timeseries;");
-console.log("res")
 console.log(res)
-console.log("res")
-// iotdb.query("select * from root.sg_test_01;");
